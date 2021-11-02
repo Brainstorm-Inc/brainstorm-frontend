8c4642c02a768da29513dc8e47d9ae26e5ed0595
Alex Petrean <49921909+alexpetrean80@users.noreply.github.com>
2021-11-02 14:47:41 +0200

    Revert "Created Topic component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopicComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
