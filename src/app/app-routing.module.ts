da6c99c07305dd0dc5311abd63c9b1e52d7b8846
sweethuman <gheo@sweethuman.tech>
2021-11-04 21:26:45 +0200

    feat(topic-page): add prototype component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TopicComponent} from "./topic/topic.component";
+import {AppversionsComponent} from "./appversions/appversions.component";
 
-const routes: Routes = [{path: 'topic', component: TopicComponent}];
+const routes: Routes = [{path: 'topic', component: TopicComponent}, {path: 'version', component: AppversionsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
