bd134e3d0349bbe34dd9c0f9856a3e42ee807e7d
Alexandru Raileanu <railalex@amazon.com>
2021-11-15 23:08:51 +0200

    Add iteration head

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopicComponent } from './topic/topic.component';
+import { IterationHeadComponent } from './iteration-head/iteration-head.component';
+import {DatePipe} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopicComponent
+    TopicComponent,
+    IterationHeadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
