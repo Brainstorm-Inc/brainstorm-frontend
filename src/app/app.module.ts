b7201989dd1bb3c9535af76d8bd822c0234bc63c
sweethuman <gheo@sweethuman.tech>
2021-11-23 17:42:22 +0200

    feat(auth): add authentication service and middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
-import { IterationHeadComponent } from './iteration-head/iteration-head.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TopicComponent} from './topic/topic.component';
+import {IterationHeadComponent} from './iteration-head/iteration-head.component';
 import {DatePipe} from "@angular/common";
+import {AuthComponent} from './auth/auth.component';
+import {ButtonComponent} from './shared/button/button.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+import {JwtInterceptor} from "./shared/helpers/jwt.interceptor";
+import {ErrorInterceptor} from "./shared/helpers/error.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicComponent,
-    IterationHeadComponent
+    IterationHeadComponent,
+    AuthComponent,
+    ButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     DatePipe
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
