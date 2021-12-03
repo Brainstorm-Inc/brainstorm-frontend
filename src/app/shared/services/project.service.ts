bb70c12fc89d1d5a59a10eb2931866b53b2a8a63
sweethuman <gheo@sweethuman.tech>
2021-12-03 14:07:51 +0200

    refactor: cleanup and organization:

--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Project} from "../model/project.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class ProjectService {
@@ -9,7 +10,7 @@ export class ProjectService {
 
   constructor(private httpClient: HttpClient){}
 
-  getProjects(): Observable<Project[]> {
-    return this.httpClient.get<Array<Project>>(this.projectsUrl);
+  getProjects(orgId: string): Observable<Project[]> {
+    return this.httpClient.get<Array<Project>>(`${environment.apiUrl}/Org/${orgId}/projects`);
   }
 }
