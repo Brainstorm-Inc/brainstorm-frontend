5223ef22841a58e09cef0184356938ae5eb7fccb
biancapitic <biapitic@yahoo.com>
2021-12-07 16:01:50 +0200

    feat: Topic component
    Added services for:
    - topic
    - iteration
    - proposal
    Added mock examples for them.

--- src/app/shared/components/sidebar/project-list/project-list.component.ts
+++ src/app/shared/components/sidebar/project-list/project-list.component.ts
@@ -1,13 +1,14 @@
 import {Component, OnInit, Pipe, PipeTransform} from '@angular/core';
 import {Project} from "../../../models/project.model";
 import {Router} from "@angular/router";
-import {Topic} from "../../shared/model/topic.model";
+import {Topic} from "../../../models/topic.model";
 import {DomSanitizer} from "@angular/platform-browser";
 
 @Pipe({name: "safeHtml"})
 export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitized: DomSanitizer) {
   }
+
   transform(value: any): any {
     return this.sanitized.bypassSecurityTrustHtml(value);
   }
@@ -18,22 +19,52 @@ export class SafeHtmlPipe implements PipeTransform {
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
 })
-export class ProjectListComponent implements OnInit{
+export class ProjectListComponent implements OnInit {
   isSelected: boolean = false;
   projects: Array<Project> = [];
   topics: Array<Topic> = [];
-  selectedProject : Project = {
+  selectedProject: Project = {
     id: '',
     name: '',
     topics: [],
   }
-  constructor(private router: Router) { }
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
-    this.projects.push({id: 'bla', name: 'pro1', topics: [{id: '1', type: 'str1', title: 'tl1', author: 'auth1', highlightedProposal: 'hh1', activeUsers: ['ncjhsbd'], currentIteration: 'iter1', iterations: ['vfd'], creationDate: '10-01-2020'}]});
-    this.projects.push({id: 'ble', name: 'pro2', topics: [{id: '2', type: 'str2', title: 'tl2', author: 'auth2', highlightedProposal: 'hh2', activeUsers: ['nbd'], currentIteration: 'iter2', iterations: ['vd'], creationDate: '10-02-2020'}]});
+    this.projects.push({
+      id: 'bla',
+      name: 'pro1',
+      topics: [{
+        id: '1',
+        type: 'str1',
+        title: 'tl1',
+        author: 'auth1',
+        highlightedProposal: 'hh1',
+        activeUsers: ['ncjhsbd'],
+        currentIteration: 'iter1',
+        iterations: ['vfd'],
+        creationDate: '10-01-2020'
+      }]
+    });
+    this.projects.push({
+      id: 'ble',
+      name: 'pro2',
+      topics: [{
+        id: '2',
+        type: 'str2',
+        title: 'tl2',
+        author: 'auth2',
+        highlightedProposal: 'hh2',
+        activeUsers: ['nbd'],
+        currentIteration: 'iter2',
+        iterations: ['vd'],
+        creationDate: '10-02-2020'
+      }]
+    });
     //this.projectService.getProjects().subscribe(projects => this.projects = projects);
-    for(let project of this.projects) {
+    for (let project of this.projects) {
       let topic = project["topics"][0];
       this.topics.push(topic);
     }
