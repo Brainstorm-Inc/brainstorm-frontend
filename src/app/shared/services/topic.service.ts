f4f2242a8bfb9583dd4543d01cbfc8bb0332dd4e
biancapitic <biapitic@yahoo.com>
2021-12-13 12:00:39 +0200

    feat: Topic service
    Added and tested requests for iterations in topic service

--- src/app/shared/services/topic.service.ts
+++ src/app/shared/services/topic.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Topic} from "../models/topic.model";
 import {environment} from "../../../environments/environment";
+import {Iteration} from "../models/iteration.model";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,14 @@ export class TopicService {
   }
 
   getTopic(topicId: string){
-    console.log(topicId)
     return this.http.get<Topic>(`${environment.apiUrl}/Topic/${topicId}`)
+  }
+
+  getTopicIterations(topicId: string) {
+    return this.http.get<Array<Iteration>>(`${environment.apiUrl}/Topic/${topicId}/iterations`);
+  }
+
+  addTopicIteration(topicId: string, iteration: Iteration) {
+    return this.http.post(`${environment.apiUrl}/Topic/${topicId}/iterations`, {iteration});
   }
 }
